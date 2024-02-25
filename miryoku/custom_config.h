16d66772b3c9af1aae01b92ffa6bb459b3dde7b8
rvogt79 <robbie.vogt@gmail.com>
2024-02-25 11:46:51 +1100

    Substitute mouse layer activation for a thumb shift.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,5 +4,5 @@
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LCTRL, A),     U_MT(LALT, R),     U_MT(LSHFT, S),    U_MT(LGUI, T),    &kp G,             &kp M,             U_MT(LGUI, N),    U_MT(LSHFT, E),    U_MT(LALT, I),     U_MT(LCTRL, O),     \
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, DEL),U_LT(U_NAV, BSPC),U_LT(U_MOUSE, RET),U_LT(U_SYM, TAB),  U_LT(U_NUM, SPACE), U_LT(U_FUN, ESC),  U_NP,              U_NP
+U_NP,              U_NP,              U_LT(U_MEDIA, DEL),U_LT(U_NAV, BSPC),U_MT(LSHFT, RET),U_LT(U_SYM, TAB),  U_LT(U_NUM, SPACE), U_LT(U_FUN, ESC),  U_NP,              U_NP
 
