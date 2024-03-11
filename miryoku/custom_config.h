a77ee3b01b815b3e0ab3771952cd81c61fb24ab0
rvogt79 <160206662+rvogt79@users.noreply.github.com>
2024-03-12 06:47:17 +1100

    Update custom_config.h with matching thumb keys for BASE and EXTRA layers.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,3 +6,8 @@ U_MT(LCTRL, A),     U_MT(LALT, R),     U_MT(LSHFT, S),    U_MT(LGUI, T),    &kp 
 U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, DEL),U_LT(U_NAV, BSPC),U_MT(LSHFT, RET),U_LT(U_SYM, TAB),  U_LT(U_NUM, SPACE), U_LT(U_FUN, ESC),  U_NP,              U_NP
 
+#define MIRYOKU_LAYER_EXTRA \
+&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
+U_MT(LCTRL, A),     U_MT(LALT, S),     U_MT(LSHFT, D),    U_MT(LGUI, F),    &kp G,             &kp H,             U_MT(LGUI, J),    U_MT(LSHFT, K),    U_MT(LALT, L),     U_MT(LCTRL, SQT),   \
+U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
+U_NP,              U_NP,              U_LT(U_MEDIA, DEL),U_LT(U_NAV, BSPC),U_MT(LSHFT, RET),U_LT(U_SYM, TAB),  U_LT(U_NUM, SPACE), U_LT(U_FUN, ESC),  U_NP,              U_NP
