4d108b81513c4f948300ed3f2884ed6fde28f429
rvogt79 <160206662+rvogt79@users.noreply.github.com>
2024-03-18 20:28:33 +1100

    Update custom_config.h: add mousekeys kludge.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,5 +1,8 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+
+#define MIRYOKU_KLUDGE_MOUSEKEYSPR
+
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LCTRL, A),     U_MT(LALT, R),     U_MT(LSHFT, S),    U_MT(LGUI, T),    &kp G,             &kp M,             U_MT(LGUI, N),    U_MT(LSHFT, E),    U_MT(LALT, I),     U_MT(LCTRL, O),     \
