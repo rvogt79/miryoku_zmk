622c32c6163b37b24618ef2b972029cd76259791
rvogt79 <160206662+rvogt79@users.noreply.github.com>
2024-03-26 20:36:49 +1100

    Second attempt at numbers.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,7 +1,10 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
+#if !defined (MIRYOKU_LAYOUTMAPPING_LILY58)
+
 #define XXX &none
-#define MIRYOKU_MAPPING( \
+
+#define MIRYOKU_LAYOUTMAPPING_LILY58( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
@@ -12,7 +15,11 @@ XXX  K00  K01  K02  K03  K04                 K05  K06  K07  K08  K09  XXX \
 XXX  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  XXX \
 XXX  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  XXX \
                XXX  K32  K33  K34       K35  K36  K37  XXX
-#undef XXX
+
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 52 53
+#define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 54 55
+
+#endif
 
 // #define MIRYOKU_KLUDGE_MOUSEKEYSPR
 
