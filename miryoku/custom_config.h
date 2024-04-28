541aecfdb2cf092ccb82ed6d9d11187505343fbf
rvogt79 <160206662+rvogt79@users.noreply.github.com>
2024-04-28 15:25:47 +1000

    Attempt to add an Engram(mer) layer on TAP.
    Uses a variant with Q and Z moved slightly.
    
    Ref: https://sunaku.github.io/engram-keyboard-layout.html#learning

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -2,18 +2,6 @@
 // https://github.com/manna-harbour/miryoku
 #if !defined (MIRYOKU_LAYOUTMAPPING_LILY58)
 
-// / {
-//   behaviors {
-//     td_shift: td_shift {
-//       compatible = "zmk,behavior-tap-dance";
-//       label = "td_shift";
-//       #binding-cells = <0>;
-//       tapping-term-ms = <1000>;
-//       bindings = <&kp LSHIFT>, <&u_caps_word>;
-//     };
-//   };
-// };
-
 #define XXX &none
 
 #define MIRYOKU_LAYOUTMAPPING_LILY58( \
@@ -27,6 +15,18 @@
 &kp MINUS  K10  K11  K12  K13  K14                 K15  K16  K17  K18  K19  &kp SEMI \
 &td_shift  K20  K21  K22  K23  K24  XXX       XXX  K25  K26  K27  K28  K29  &td_shift \
                &kp LS(LG(P))  K32  K33  K34       K35  K36  K37  XXX
+
+#define MIRYOKU_LAYERMAPPING_TAP( \
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+) \
+&kp EQUAL  &kp N1  &kp N2  &kp N3  &kp N4  &kp N5                 &kp N6  &kp N7  &kp N8  &kp N9  &kp N0  &kp BSLH \
+&kp LBKT  &kp B  &kp Y  &kp O  &kp U  &kp APOS                 &kp SEMI  &kp L  &kp D  &kp W  &kp V  &kp RBKT \
+&kp Z  &kp C  &kp I  &kp E  &kp A  &kp COMMA                 &kp PERIOD  &kp H  &kp T  &kp S  &kp N  &kp Q \
+&td_shift  &kp G  &kp X  &kp J  &kp K  &kp MINUS  XXX       XXX  &kp FLSH  &kp R  &kp M  &kp F  &kp P  &td_shift \
+               &kp LS(LG(P))  &kp DEL  &kp BSPC  &kp RET       &kp TAB  &kp SPC  &kp ESC  &to BASE
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 52 53
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 54 55
